ebff166ad60bf1cbfbb60b43779a305ec49f84a4
TRIVEN\USER <triven.n33@gmail.com>
2026-04-30 20:23:19 -0400

    Fix indentation and class name in UsersController for consistency

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,12 @@
 module Api
-    module V1
-        class UserController < ApplicationController
-            def me
-                render json: {
-                    id: current_user.id,
-                    email: current_user.email
-                }
-            end
-        end
-    end 
+  module V1
+    class UsersController < ApplicationController
+      def me
+        render json: {
+          id: current_user.id,
+          email: current_user.email
+        }
+      end
+    end
+  end
 end
