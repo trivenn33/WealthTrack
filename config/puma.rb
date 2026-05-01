3c5569b91fbb99328d68bd0345b5326aa8cffa04
TRIVEN\USER <triven.n33@gmail.com>
2026-04-30 20:16:48 -0400

    Reorder Puma configuration for clarity and add environment setting

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,12 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
+workers 0
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
 
-plugin :tmp_restart
+environment ENV.fetch("RAILS_ENV") { "development" }
 
+plugin :tmp_restart
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
